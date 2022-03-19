8deda76600d8b5faa39fcd2dcc4bff19e6e2e6f8
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2022-03-19 14:24:12 +0300

    refactor

--- class_Money.py
+++ class_Money.py
@@ -1,5 +1,3 @@
-import time
-
 from save_data import data, day, month, year
 
 
@@ -18,6 +16,8 @@ class Money:
     Методы класса:
     convert_to_usd(): Конвертирует денежные средства, находящиеся на счете в доллары США
     convert_to_valute(): Конвертирует денежные средства, находящиеся на счете в любую валюту из списка
+    _convert_to_valute_for_compare(): Конвертирует денежные средства, находящиеся на счете в любую валюту из списка
+    для методов сравнения
     """
     def __init__(self, value: (int, float), charcode: str = "RUB"):
         """
@@ -69,31 +69,46 @@ class Money:
 
     def __gt__(self, other: "Money"):
         if isinstance(other, Money):
-            return self.value > other.value
+            if self.charcode == other.charcode:
+                return self.value > other.value
+            else:
+                return self.value > other._convert_to_valute_for_compare(self.charcode)
         else:
             return NotImplemented
 
     def __lt__(self, other: "Money"):
         if isinstance(other, Money):
-            return self.value < other.value
+            if self.charcode == other.charcode:
+                return self.value < other.value
+            else:
+                return self.value < other._convert_to_valute_for_compare(self.charcode)
         else:
             return NotImplemented
 
     def __ge__(self, other: "Money"):
         if isinstance(other, Money):
-            return self.value >= other.value
+            if self.charcode == other.charcode:
+                return self.value >= other.value
+            else:
+                return self.value >= other._convert_to_valute_for_compare(self.charcode)
         else:
             return NotImplemented
 
     def __le__(self, other: "Money"):
         if isinstance(other, Money):
-            return self.value <= other.value
+            if self.charcode == other.charcode:
+                return self.value <= other.value
+            else:
+                return self.value <= other._convert_to_valute_for_compare(self.charcode)
         else:
             return NotImplemented
 
     def __eq__(self, other: "Money"):
         if isinstance(other, Money):
-            return self.value == other.value
+            if self.charcode == other.charcode:
+                return self.value == other.value
+            else:
+                return self.value == other._convert_to_valute_for_compare(self.charcode)
         else:
             return NotImplemented
 
@@ -132,7 +147,8 @@ class Money:
 
     def convert_to_valute(self, valute: str):
         """
-        Метод онвертирует денежные средства, находящиеся на счете в любую валюту из списка
+        Метод конвертирует денежные средства, находящиеся на счете в любую валюту из списка
+        Данные используются только для сравнения разных валют. Атрибуты не изменяются
         Для выбора спользуются международные общепринятые наименования (смотри файл price)
 
         :param valute: Валюта, в которую нужно перевести средства, находящиеся на счете
@@ -143,13 +159,24 @@ class Money:
             return None
 
         elif valute in data["Valute"]:
-            value1 = data["Valute"][self.charcode]["Value"] / data["Valute"][self.charcode]["Nominal"]
-            value2 = data["Valute"][valute]["Value"] / data["Valute"][valute]["Nominal"]
-            self.value = round(self.value * value1 / value2, 2)
-            self.charcode = valute
-            self.date = f"Данные на {day}.{month}.{year}"
-            print(f"Конвертация в USD прошла по курсу на {day}.{month}.{year}\n{self.value} {self.charcode}")
-            return self.value
+            if self.charcode == "RUB":
+                value1 = data["Valute"][valute]["Value"] / data["Valute"][valute]["Nominal"]
+                self.value = round(self.value / value1, 2)
+                self.charcode = valute
+                self.date = f"Данные на {day}.{month}.{year}"
+                print(f"Конвертация в {self.charcode} прошла по курсу "
+                      f"на {day}.{month}.{year}\n{self.value} {self.charcode}")
+                return self.value
+
+            else:
+                value1 = data["Valute"][self.charcode]["Value"] / data["Valute"][self.charcode]["Nominal"]
+                value2 = data["Valute"][valute]["Value"] / data["Valute"][valute]["Nominal"]
+                self.value = round(self.value * value1 / value2, 2)
+                self.charcode = valute
+                self.date = f"Данные на {day}.{month}.{year}"
+                print(f"Конвертация в {self.charcode} прошла по курсу "
+                      f"на {day}.{month}.{year}\n{self.value} {self.charcode}")
+                return self.value
 
         elif valute == "RUB":
             value1 = data["Valute"][self.charcode]["Value"]
@@ -157,8 +184,35 @@ class Money:
             self.value = round(self.value * value1 / value2, 2)
             self.charcode = valute
             self.date = f"Данные на {day}.{month}.{year}"
-            print(f"Конвертация в USD прошла по курсу на {day}.{month}.{year}\n{self.value} {self.charcode}")
+            print(f"Конвертация в {self.charcode} прошла по курсу "
+                  f"на {day}.{month}.{year}\n{self.value} {self.charcode}")
             return self.value
+
+        else:
+            raise TypeError("Валюты, в которую вы хотите конвертировать свои деньги, нет в обменнике")
+
+    def _convert_to_valute_for_compare(self, valute: str):
+        """
+        Метод конвертирует денежные средства, находящиеся на счете в любую валюту из списка, без изменения самого счета
+        Для выбора спользуются международные общепринятые наименования (смотри файл price)
+
+        :param valute: Валюта, в которую нужно перевести средства, находящиеся на счете
+        :return: возвращает сумму на счете в нужной валюте
+        """
+
+        if valute in data["Valute"]:
+            value1 = data["Valute"][self.charcode]["Value"] / data["Valute"][self.charcode]["Nominal"]
+            value2 = data["Valute"][valute]["Value"] / data["Valute"][valute]["Nominal"]
+            new_value = round(self.value * value1 / value2, 2)
+            print(f"Сравнение прошло по курсу на {day}.{month}.{year}")
+            return new_value
+
+        elif valute == "RUB":
+            value1 = data["Valute"][self.charcode]["Value"]
+            value2 = data["Valute"][self.charcode]["Nominal"]
+            new_value = round(self.value * value1 / value2, 2)
+            print(f"Сравнение прошло по курсу на {day}.{month}.{year}")
+            return new_value
 
         else:
             raise TypeError("Валюты, в которую вы хотите конвертировать свои деньги, нет в обменнике")
@@ -166,11 +220,34 @@ class Money:
 
 if __name__ == '__main__':
 
-    money1 = Money(300, "CZK")
-    money2 = Money(324.56, "USD")
-    money3 = Money(770.99, "EUR")
+    bank_account1 = Money(3000, "CZK")
+    bank_account2 = Money(150, "USD")
+    bank_account3 = Money(200, "EUR")
 
-    print(money1)
-    money1.convert_to_valute("EUR")
-    print(money1.date)
-    print(money1 < money2)
+    print("------------")
+    print(bank_account1)
+    print(bank_account2)
+    print(bank_account3)
+    print("------------")
+    bank_account1.convert_to_valute("RUB")
+    print("------------")
+    print(bank_account1.date)
+    print("------------")
+    print(bank_account1 > bank_account2)
+    print("------------")
+    print(bank_account1)
+    print(bank_account2)
+    print(bank_account3)
+    print("------------")
+    print(bank_account1 < bank_account3)
+    print("------------")
+    bank_account1.convert_to_valute("CZK")
+    print("------------")
+    bank_account2.convert_to_valute("EUR")
+    print("------------")
+    bank_account2.convert_to_usd()
+    print("------------")
+    print(bank_account1)
+    print(bank_account2)
+    print(bank_account3)
+    print("------------")
